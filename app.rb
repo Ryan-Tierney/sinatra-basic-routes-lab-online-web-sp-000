179bbd5f938814ca98172e3cde4a51723a2f4940
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-14 04:53:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get 'name' do 
     "My name is Ryan"
-  end 
+  
   get 'hometown' do 
     "My hometown is Waveland" 
    
   get 'favorite-song'
    "My favorite song is Night on the Town"
-   
+  end 
 end
